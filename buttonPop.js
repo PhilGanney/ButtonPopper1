08ecaee8a2a7d41ca8c372c7b68807903fb7d72d
Phil Ganney <philipganney@gmail.com>
2021-12-20 01:27:05 +0000

    Pausing and unpausing the tick
    More complex than I'd initially anticipated.
    
    This is because setting t on each call of startButton would reset the count each pause and unpause - and we don't want that since t will be used to determine the difficulty at a given moment. Now perhaps we could just set t to 0 on it's initialisation, and that would remove the need for the extra block, but I'm unsure at this point if that would then work when players restart after a gameover.

--- buttonPop.js
+++ buttonPop.js
@@ -1,10 +1,32 @@
 var gameTick;
 var t;
+var gameState = "notStarted"; //running, paused, notStarted, gameOver
 
-function start(){
-	t = 0; //t for time elapsed
-	gameTick = setInterval(function(){
-		t++;
-		document.getElementById("HS").innerHTML =  t; //Temporarily displaying the tick where High Scores will go as a way of proving tick is happening
-	}, 1000);
+function startBtn(){
+	//This function is run on the button that starts the game, that same button will pause it if the game is running and unpause if the game is paused
+	if (gameState == "running") { //this must be the first check on the button, so that pauses are as instantaneous as possible
+		pause();
+	} else if (gameState == "paused") {
+		gameTick = setInterval(function(){
+			t++;
+			document.getElementById("HS").innerHTML =  t; //Temporarily displaying the tick where High Scores will go as a way of proving tick is happening
+		}, 1000);
+		gameState = "running";
+		document.getElementById("startBtn").innerHTML = "pause";
+	} else if (gameState == "notStarted") {
+		t = 0; //t for time elapsed
+		gameTick = setInterval(function(){
+			t++;
+			document.getElementById("HS").innerHTML =  t; //Temporarily displaying the tick where High Scores will go as a way of proving tick is happening
+		}, 1000);
+		gameState = "running";
+		document.getElementById("startBtn").innerHTML = "pause";
+	}
+}
+
+
+function pause(){
+	clearInterval(gameTick);
+	gameState = "paused";
+	document.getElementById("startBtn").innerHTML = "unpause";
 }
