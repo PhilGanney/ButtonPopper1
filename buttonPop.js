f0862b70b0fe3be5c3b31ab07b1df7fbb9abd648
Phil Ganney <philipganney@gmail.com>
2021-12-20 00:45:21 +0000

    gameTick starts
    gameTick is how we make the game time based.
    
    In this commit I'm displaying the number of ticks that have elapsed where the high score will be shown, just as a simple of proving that the ticks are happening - it's just a sanity check.

--- buttonPop.js
+++ buttonPop.js
@@ -1,4 +1,10 @@
+var gameTick;
+var t;
+
 function start(){
-	alert("go");
-	
+	t = 0; //t for time elapsed
+	gameTick = setInterval(function(){
+		t++;
+		document.getElementById("HS").innerHTML =  t; //Temporarily displaying the tick where High Scores will go as a way of proving tick is happening
+	}, 1000);
 }
