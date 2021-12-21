16e96525f83a341e6d0e3ac8d6efb80d4f507399
Phil Ganney <philipganney@gmail.com>
2021-12-21 01:52:48 +0000

    clicking buttons returns them to normal

--- buttonPop.js
+++ buttonPop.js
@@ -22,7 +22,7 @@ function play(){
 			btn.innerHTML = "+1";
 			btn.classList.add("green");
 			btn.classList.remove("silver");
-			btn.onclick = function() {goodBtn(1);} //can't put btn.onclick = goodBtn(1); as that would immediatly call the function, thought I could do btn.addEventListener("click", goodBtn, 1); but that doesn't actually work and it seems the only way to get addEventListener to work with a param is to use a polyfiller or an anonymous function anyway, so there's no advantage I can see
+			btn.onclick = function() {goodBtn(1, this);} //Multiple notes on this line a) this uses an anonymous function to call our main function, but we need the main function to know what button is calling it, which it wouldn't be able to get from the "this" keyword, so we pass it here. b) can't put btn.onclick = goodBtn(1); as that would immediatly call the function, thought I could do btn.addEventListener("click", goodBtn, 1); but that doesn't actually work and it seems the only way to get addEventListener to work with a param is to use a polyfiller or an anonymous function anyway, so there's no advantage I can see
 			//document.getElementById("HS").innerHTML =  t; //Temporarily displaying the tick where High Scores will go as a way of proving tick is happening
 		}, 1000); //Using 1000 for now to run this once per second. It's hard to know how often this should run, and I think I'm going to need to do some trial and error at some point once I have more stuff built. 33 would make the game run at roughly 30 gameTicks per Second, which I was thinking would be ideal because of the 30fps standard for games that don't need super responsiveness, but this isn't frames of animation!
 	gameState = "running";
@@ -42,7 +42,13 @@ function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-function goodBtn(n){
+function goodBtn(n, btn){
+	//n is an integer that we add to the score
+	//btn is the button - this function is called indirectly via an anonymous function, so using the "this" keyword in this function wouldn't get the button but we can use "this" in the anonymous function and pass it down
 	let cs = document.getElementById("CS");
 	cs.innerHTML = (parseInt(cs.innerHTML) + n) + ""; //innerHTML is a string, but we want to do integer addition to it, so this takes the innerHTML as a string, converts it to integer, adds n, then converts that result back to a string via string concatenation with an empty string, and finally sets it back into the innerHTML
+
+	btn.classList.remove("green");
+	btn.classList.add("silver");
+	btn.innerHTML = "0";
 }
