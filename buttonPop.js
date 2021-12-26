61893f59b93ecfab2082dd0345638c721f30a97e
Phil Ganney <philipganney@gmail.com>
2021-12-26 18:38:14 +0000

    Alert when player reaches target score
    Target score reduced to 20 to make it easy to test code that triggers at that point

--- buttonPop.js
+++ buttonPop.js
@@ -47,11 +47,16 @@ function goodBtn(n, btn){
 		//n is an integer that we add to the score
 		//btn is the button - this function is called indirectly via an anonymous function, so using the "this" keyword in this function wouldn't get the button but we can use "this" in the anonymous function and pass it down
 		let cs = document.getElementById("CS");
+		let ts = document.getElementById("TS");
 		cs.innerHTML = (parseInt(cs.innerHTML) + n) + ""; //innerHTML is a string, but we want to do integer addition to it, so this takes the innerHTML as a string, converts it to integer, adds n, then converts that result back to a string via string concatenation with an empty string, and finally sets it back into the innerHTML
 
 		btn.classList.remove("green");
 		btn.classList.add("silver");
 		btn.innerHTML = "0";
+		if(parseInt(cs.innerHTML) == parseInt(ts.innerHTML)){
+			pause();
+			alert("Boom! Target score reached");
+		}
 	}
 }
 
